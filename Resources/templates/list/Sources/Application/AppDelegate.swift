a0bd662b70f4fa68ee38244255f26bfdd288647e
Eric Marchand <eric.marchand@4d.com>
2019-09-04 08:54:23 +0100

    [Mobile] Add iOS 13 new UIScene delegate code in app template #109559 Activated only if compiled with swift 5.1(Xcode11)
    [git-p4: depot-paths = "//depot/4eDimension/main/4DComponents/Internal User Components/4D Mobile App/": change = 241137]

--- Resources/templates/list/Sources/Application/AppDelegate.swift
+++ Resources/templates/list/Sources/Application/AppDelegate.swift
@@ -9,7 +9,6 @@ import UIKit
 
 import QMobileDataStore
 import QMobileUI
-
 import XCGLogger
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,9 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
-       // UIApplication.shared.statusBarStyle = .lightContent
-
         return true
     }
 
@@ -48,4 +44,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Saves changes in the application's managed object context before the application terminates.
     }
 
+    // MARK: UISceneSession Lifecycle
+    #if swift(>=5.1)
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    #endif
 }
