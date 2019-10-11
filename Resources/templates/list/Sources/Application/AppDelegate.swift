86c8868d6172efde0e56f78b8cdd4557a0217a9e
Eric Marchand <eric.marchand@4d.com>
2019-10-11 13:32:25 +0100

    [Mobile] Fix some feedback and crash dialog display #108873 Action UIScene and fix bug about it #110248
    Jobs: redmine:108873
    [git-p4: depot-paths = "//depot/4eDimension/main/4DComponents/Internal User Components/4D Mobile App/": change = 242892]

--- Resources/templates/list/Sources/Application/AppDelegate.swift
+++ Resources/templates/list/Sources/Application/AppDelegate.swift
@@ -46,7 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
     #if swift(>=5.1)
-    /*@available(iOS 13.0, *)
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -58,6 +58,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }*/
+    }
     #endif
 }
