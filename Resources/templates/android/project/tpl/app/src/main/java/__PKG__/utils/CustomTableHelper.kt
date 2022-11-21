c5c83fe63371c1c15d81b4e63f98e3ddb600b1d8
houssam <houssam.temanni@4D.com>
2022-11-21 17:33:56 +0100

    Rename SectionFieldClass to QueryField

--- Resources/templates/android/project/tpl/app/src/main/java/__PKG__/utils/CustomTableHelper.kt
+++ Resources/templates/android/project/tpl/app/src/main/java/__PKG__/utils/CustomTableHelper.kt
@@ -102,10 +102,10 @@ class CustomTableHelper : GenericTableHelper {
     /**
      * Provides the appropriate SectionField
      */
-    override fun getSectionFieldForTable(tableName: String): SectionField? {
+    override fun getSectionFieldForTable(tableName: String): QueryField? {
         return when (tableName) {
             {{#section_fields}}
-            "{{tableName}}" -> SectionField("{{name}}", "{{valueType}}", "{{path}}")
+                "{{tableName}}" -> QueryField("{{name}}", "{{valueType}}", "{{path}}")
             {{/section_fields}}
             else -> null
         }
@@ -114,10 +114,10 @@ class CustomTableHelper : GenericTableHelper {
     /**
      * Provides the appropriate default sort field
      */
-    override fun getDefaultSortFieldForTable(tableName: String): SectionField? {
+    override fun getDefaultSortFieldForTable(tableName: String): QueryField? {
         return when (tableName) {
             {{#default_sort_fields}}
-                "{{tableName}}" -> SectionField("{{name}}", "{{valueType}}", null)
+                "{{tableName}}" -> QueryField("{{name}}", "{{valueType}}", null)
             {{/default_sort_fields}}
             else -> null
         }
